48a949e6a2778c49a9a218b2a9b2bfe3fa904e61
tomnir <tomnir93@gmail.com>
2026-07-08 16:29:51 +0200

    function signatures adjusted (made things const)

--- types/Vec3.h
+++ types/Vec3.h
@@ -14,21 +14,18 @@ private:
 public:
         // Object mgmt
         Vec3();
+        Vec3(const GLfloat firstComponent, const GLfloat secondComponen, const GLfloat thirdComponent);
         Vec3(Vec3);
-        ~Vec3();
         // Operators
-        friend Vec3 operator*(Vec3& lhs, Vec3& rhs);
-        friend Vec3 operator+(Vec3& lhs, Vec3& rhs);
-        friend Vec3 operator-(Vec3& lhs, Vec3& rhs);
-        Vec3 operator*(double rhs);
-        friend Vec3 operator*(Vec3& lhs, double rhs);
-        Vec3 operator=(Vec3& rhs);
-        friend bool operator==(Vec3& lhs, Vec3& rhs)
-        GLfloat& operator() (size_t column, size_t row);
+        friend Vec3 operator*(const Vec3& lhs, const Vec3& rhs);
+        friend Vec3 operator+(const Vec3& lhs, const Vec3& rhs);
+        friend Vec3 operator-(const Vec3& lhs, const Vec3& rhs);
+        Vec3 operator*(const double rhs);
+        Vec3& operator=(const Vec3& rhs);
+        friend bool operator==(const Vec3& lhs, const Vec3& rhs);
+        GLfloat& operator() (const size_t component);
         // Methods
         GLfloat getLength();
-        GLfloat scalarProduct(Vec3& rhs);
+        GLfloat scalarProduct(const Vec3& rhs);
 };
-
-
 #endif //CG1PROJEKT_VEC3_H
