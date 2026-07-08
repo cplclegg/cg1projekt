35d21883571ab3b47ee37679c8d838af1a8af90c
tomnir <tomnir93@gmail.com>
2026-07-08 19:45:49 +0200

    fixes according to test results v1

--- types/Vec3.cpp
+++ types/Vec3.cpp
@@ -4,66 +4,47 @@
 
 #include "Vec3.h"
 #include <cmath>
-#include <GL/glew>
-#include <gl/glfw>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 using namespace std;
-
-// Constructors
-
-Vec3::Vec3()
-    : m_dimension {3}
-    , m_vector {0.0f, 0.0f, 0.0f}
-{
-    for (GLfloat& component : m_vector)
-    {
-        component = 0.0f;
-    }
-}
-
-Vec3::Vec3(const GLfloat firstComponent, const GLfloat secondComponent, const GLfloat thirdComponent)
-    : m_dimension {3}
-    , m_vector {firstComponent, secondComponent, thirdComponent}
-{
-}
-
-Vec3::Vec3(const Vec& other)
-    : m_dimension {3}
-    , m_vector {}
-{
-    for (int i = 0; i < m_dimension; ++i)
-    {
-        m_vector[i] = other(i);
-    }
-}
 
 // Operators
 
-GLfloat& operator() (const size_t component)
+GLfloat& Vec3::operator() (const int component)
+{
+    return m_vector[component];
+}
+
+const GLfloat& Vec3::operator() (const int component) const
 {
     return m_vector[component];
 }
 
 Vec3 operator*(const Vec3& rhs, const Vec3& lhs)
 {
-    Vec3 result;
-    result(0) = lhs(1)*rhs(2) - lhs(2)*rhs(1);
-    result(1) = lhs(2)*rhs(0) - lhs(0)*rhs(2);
-    result(2) = lhs(0)*rhs(1) - lhs(1)*rhs(0);
+    Vec3 result {};
+    result(0) = lhs.m_vector[1]*rhs.m_vector[2] - lhs.m_vector[2]*rhs.m_vector[1];
+    result(1) = lhs.m_vector[2]*rhs.m_vector[0] - lhs.m_vector[0]*rhs.m_vector[2];
+    result(2) = lhs.m_vector[0]*rhs.m_vector[1] - lhs.m_vector[1]*rhs.m_vector[0];
     return result;
 }
 
-Vec3 Vec3::operator*(const double rhs)
+Vec3 Vec3::operator*(const GLfloat rhs) const
 {
-    Vec3 result;
-    result(0) = m_vector[1]*rhs(2) - m_vector[2]*rhs(1);
-    result(1) = m_vector[2]*rhs(0) - m_vector[0]*rhs(2);
-    result(2) = m_vector[0]*rhs(1) - m_vector[1]*rhs(0);
-    return result;
+    return Vec3 {
+    m_vector[0] * (GLfloat) rhs,
+    m_vector[1] * (GLfloat) rhs,
+    m_vector[2] * (GLfloat) rhs
+    };
 }
 
 Vec3 operator+(const Vec3& lhs, const Vec3& rhs)
 {
-    return vec3( lhs(0)+rhs(0), lhs(1)+rhs(1), rhs(2)+lhs(2) );
+    Vec3 result {};
+    result(0) = lhs(0)+rhs(0);
+    result(1) = lhs(1)+rhs(1);
+    result(2) = lhs(2)+rhs(2);
+    return result;
 }
 
 Vec3 operator-(const Vec3& lhs, const Vec3& rhs)
@@ -71,14 +52,39 @@ Vec3 operator-(const Vec3& lhs, const Vec3& rhs)
     return Vec3( lhs(0)-rhs(0), lhs(1)-rhs(1), rhs(2)-lhs(2) );
 }
 
-Vec3& Vec3::operator=(const Vec3& rhs)
+Vec3 Vec3::operator=(const Vec3& rhs)
 {
-    return Vec3(rhs);
+    Vec3 result {};
+    result(0)=rhs(0);
+    result(1)=rhs(1);
+    result(2)=rhs(2);
+    return result;
 }
 
 bool operator==(const Vec3& lhs, const Vec3& rhs)
 {
     return (lhs(0) == rhs(0) && lhs(1) == rhs(1) && lhs(2) == rhs(2));
+}
+
+// Constructors
+
+Vec3::Vec3()
+    : m_vector {0.0f, 0.0f, 0.0f}
+{
+}
+
+Vec3::Vec3(const GLfloat firstComponent, const GLfloat secondComponent, const GLfloat thirdComponent)
+    : m_vector {firstComponent, secondComponent, thirdComponent}
+{
+}
+
+Vec3::Vec3(const Vec3& other)
+    : m_vector {0.0f, 0.0f, 0.0f}
+{
+    for (int i = 0; i < m_dimension; ++i)
+    {
+        m_vector[i] = other(i);
+    }
 }
 
 // Methods
@@ -90,7 +96,7 @@ GLfloat Vec3::getLength()
 
 GLfloat Vec3::scalarProduct(const Vec3& rhs)
 {
-    return ( lhs(0)*rhs(0) + lhs(1)*rhs(1) + lhs(2)*rhs(2) );
+    return ( m_vector[0]*rhs(0) + m_vector[1]*rhs(1) + m_vector[2]*rhs(2) );
 }
 
 
