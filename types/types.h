189574de99754ea333a3abf9cbca8a933d3494c0
tomnir <tomnir93@gmail.com>
2026-07-09 13:13:06 +0200

    created types.h to consolidate importing custom types for this project

---
+++ types/types.h
@@ -0,0 +1,10 @@
+//
+// Created by tomnir on 09.07.26.
+//
+
+#ifndef CG1PROJEKT_TYPES_H
+#define CG1PROJEKT_TYPES_H
+#include "Vec3.h"
+#include "Vec4.h"
+#include "Mat4.h"
+#endif //CG1PROJEKT_TYPES_H
