77f719a009c97e70d0e03d36be35ac9e64ebb981
tomnir <tomnir93@gmail.com>
2026-08-04 18:47:11 +0200

    First draft of class complete. Since last commit: implemented constructors and destructor, implemented getBuffer(), implemented makeVBO()

--- src/utilities/ObjectData.cpp
+++ src/utilities/ObjectData.cpp
@@ -6,11 +6,49 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <cassert>
+#include <stdexcept>
 
 #include <bits/error_constants.h>
 using namespace std;
 
+ObjectData::ObjectData()
+{
+}
 
+ObjectData::ObjectData(const ObjectData& other)
+    : m_vertexCount {other.m_vertexCount}
+    , m_vCount {other.m_vCount}
+    , m_vtCount {other.m_vtCount}
+    , m_vnCount {other.m_vnCount}
+    , m_fCount {other.m_fCount}
+    , m_lineLength {other.m_lineLength}
+    , m_buffer {other.m_buffer}
+{
+}
+
+ObjectData::ObjectData(const std::string& path)
+    : m_buffer {loadObj(path.c_str())}
+{
+}
+
+ObjectData::~ObjectData() // do NOT delete or free m_buffer, its heap allocated
+{
+}
+
+GLfloat* ObjectData::getBuffer() const
+{
+    return m_buffer;
+}
+
+void ObjectData::makeVBO(GLuint& vbo) const
+{
+    assert(m_buffer != nullptr);
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, m_bufferSize, m_buffer, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
 
 GLfloat* ObjectData::loadObj(const char* location)
 {
@@ -20,27 +58,28 @@ GLfloat* ObjectData::loadObj(const char* location)
     ssize_t read = 0;
 
     // line buffer for getline
-    char* line = (char*)malloc(m_lineLength);
+    char* line = static_cast<char*>(malloc(m_lineLength));
 
     // count number of lines
     m_vCount = 0;
     m_vtCount = 0;
     m_vnCount = 0;
     m_fCount = 0;
-    while ((read = getline(&line, &lineLength, file)) != -1) {
-        if (strncmp(line, "v ", 2) == 0) ++vCount;
-        else if (strncmp(line, "vt ", 3) == 0) ++vtCount;
-        else if (strncmp(line, "vn ", 3) == 0) ++vnCount;
-        else if (strncmp(line, "f ", 2) == 0) ++fCount;
+    while ((read = getline(&line, &m_lineLength, file)) != -1) {
+        if (strncmp(line, "v ", 2) == 0) ++m_vCount;
+        else if (strncmp(line, "vt ", 3) == 0) ++m_vtCount;
+        else if (strncmp(line, "vn ", 3) == 0) ++m_vnCount;
+        else if (strncmp(line, "f ", 2) == 0) ++m_fCount;
     }
 
     // allocate temporary buffers
-    Vector3* v = malloc(m_vCount * sizeof(Vector3));
-    Vector2* vt = malloc(m_vtCount * sizeof(Vector2));
-    Vector3* vn = malloc(m_vnCount * sizeof(Vector3));
+    Vector3* v  = static_cast<Vector3*>(malloc(m_vCount * sizeof(Vector3)));
+    Vector2* vt = static_cast<Vector2*>(malloc(m_vtCount * sizeof(Vector2)));
+    Vector3* vn = static_cast<Vector3*>(malloc(m_vnCount * sizeof(Vector3)));
 
     // allocate output buffer
-    Triangle* buffer = malloc(m_fCount * sizeof(Triangle));
+    m_bufferSize = m_fCount * sizeof(Triangle);
+    Triangle* buffer = static_cast<Triangle*>(malloc(m_bufferSize));
 
     // reset counters
     m_vCount = 0;
