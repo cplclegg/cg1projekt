1174453c8fea2c3a6c0aac551246b01e3ae62bee
tomnir <tomnir93@gmail.com>
2026-08-10 21:41:54 +0200

    added class that provides a static method combining the meson written root directory for the project with a given relative resource path to an absolute path suitable for file opening

---
+++ src/utilities/ResourceLocator.cpp
@@ -0,0 +1,10 @@
+//
+// Created by tomnir on 10.08.26.
+//
+
+#include "ResourceLocator.h"
+#include <iostream>
+std::filesystem::path ResourceLocator::getResourcePath(const std::filesystem::path& relativePath)
+{
+    return std::filesystem::path { PROJECT_SOURCE_ROOT/relativePath };
+}
