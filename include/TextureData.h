d7144ace502ea33cf77b4a2174fd410c4c33530c
tomnir <tomnir93@gmail.com>
2026-08-10 23:28:13 +0200

    first draft of TextureData.cpp

---
+++ include/TextureData.h
@@ -0,0 +1,40 @@
+//
+// Created by tomnir on 10.08.26.
+//
+
+#ifndef CG1PROJEKT_TEXTUREDATA_H
+#define CG1PROJEKT_TEXTUREDATA_H
+
+#include "../../include/ResourceLocator.h"
+#include <iostream>
+#include <filesystem>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+class TextureData
+{
+private:
+    int m_width = 0;
+    int m_height = 0;
+    int m_channels = 0;
+    unsigned char *m_imageData = nullptr;
+    std::filesystem::path m_location;
+    bool m_created = false;
+    GLenum m_target = GL_TEXTURE_2D;
+    GLuint m_textureName;
+
+public:
+
+    explicit TextureData(const std::filesystem::path& relativePath);
+    explicit TextureData(const std::filesystem::path& relativePath, GLenum target);
+
+    void loadImageData();
+
+    GLuint createTexture();
+    GLuint getTextureName();
+};
+
+
+#endif //CG1PROJEKT_TEXTUREDATA_H
---
+++ src/utilities/TextureData.cpp
@@ -0,0 +1,74 @@
+//
+// Created by tomnir on 10.08.26.
+//
+
+#include "../../include/TextureData.h"
+
+using namespace std;
+
+TextureData::TextureData(const std::filesystem::path& relativePath)
+    : m_location { ResourceLocator::getResourcePath(relativePath) }
+    , m_textureName {}
+{
+    loadImageData();
+}
+
+TextureData::TextureData(const std::filesystem::path& relativePath, GLenum target)
+    : m_location { ResourceLocator::getResourcePath(relativePath) }
+    , m_target {target}
+    , m_textureName {}
+{
+    loadImageData();
+}
+
+void TextureData::loadImageData()
+{
+    m_imageData = stbi_load(m_location.c_str(), &m_width, &m_height, &m_channels, 0);
+    if (m_imageData == nullptr)
+    {
+        cout << "Error loading image data from " << m_location << " " << endl;
+    }
+}
+
+GLuint TextureData::createTexture()
+{
+    glGenTextures(1, &m_textureName);
+    glBindTexture(m_target, m_textureName);
+    switch (m_target) // in case of future use of 1d/3d texture expand cases to adjust
+    {
+    case GL_TEXTURE_2D:
+        glTexImage2D(
+            m_target,
+        0,
+        GL_RGB,
+            m_width,
+            m_height,
+            0,
+            GL_RGB,
+            GL_UNSIGNED_BYTE,
+            m_imageData
+        );
+        break;
+    default:
+        break;
+    }
+    glGenerateMipmap(m_target);
+    m_created = true;
+    stbi_image_free(m_imageData);
+    glBindTexture(m_target, 0);
+    return m_textureName;
+}
+
+GLuint TextureData::getTextureName()
+{
+    if (m_created)
+    {
+        return m_textureName;
+    } else
+    {
+        createTexture();
+        return m_textureName;
+    }
+
+}
+
