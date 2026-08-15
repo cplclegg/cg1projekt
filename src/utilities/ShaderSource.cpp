eba8a285c23e5f4d43216eb6ac07597e674109a2
tomnir <tomnir93@gmail.com>
2026-08-15 11:58:18 +0200

    removed commented out deprecated function

--- src/utilities/ShaderSource.cpp
+++ src/utilities/ShaderSource.cpp
@@ -45,28 +45,9 @@ void ShaderSource::loadSourceFile(const string& path)
     buffer = readSourceFile(path);
     loadSuccess = true;
 }
-/*
-char* ShaderSource::readSourceFile(const string& path)
-{
-    assert(!path.empty());
-
-    ifstream fs {path, ifstream::in};
-    if (!fs.is_open()) throw runtime_error("Error opening shader file: " + path);
-
-    fs.seekg(0,ifstream::end);
-    const auto length {fs.tellg()};
-    if (length == streampos(-1)) throw runtime_error("Error determining shader file size: " + path);
-    fs.seekg(0,ifstream::beg);
-
-    const auto buf = static_cast<char*>(malloc(sizeof(char)*length));
-    if(!fs.read(buf, length)) throw runtime_error("Error reading shader source code from: " + path);
-    fs.close();
-    return buf;
-}*/
 
 char* ShaderSource::readSourceFile(const std::filesystem::path& relativePath)
 {
-    //assert(!path.empty());
     std::filesystem::path path {ResourceLocator::getResourcePath(relativePath)};
     ifstream fs {path, ifstream::in};
     if (!fs.is_open()) throw runtime_error("Error opening shader file: " + path.string());
