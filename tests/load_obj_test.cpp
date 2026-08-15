c867af259d9c15280911015564b9dd4429afb23f
tomnir <tomnir93@gmail.com>
2026-08-15 11:59:56 +0200

    changed shader locations to new testshaders subdirectory

--- tests/load_obj_test.cpp
+++ tests/load_obj_test.cpp
@@ -1,7 +1,7 @@
-#include "../include/ObjectData.h"
-#include "../include/ShaderSource.h"
-#include "../include/ShaderProgram.h"
-#include "../include/Mat4.h"
+#include "../../include/ObjectData.h"
+#include "../../include/ShaderSource.h"
+#include "../../include/ShaderProgram.h"
+#include "../../include/Mat4.h"
 
 
 /*Struct notwendig um sowohl Programm-ID, Vertex-Anzahl als auch VAO
@@ -14,7 +14,7 @@ struct RenderData {
 };
 
 RenderData init(void) {
-    ShaderProgram shader{"src/shaders/VertexShader.glsl", "src/shaders/FragmentShader.glsl"};
+    ShaderProgram shader{"src/shaders/testshaders/FragmentShader.glsl", "src/shaders/testshaders/VertexShader.glsl"};
     ObjectData testObject{"tests/teapot.obj"};
     GLuint vbo = testObject.makeVBO();
     GLuint vao;
