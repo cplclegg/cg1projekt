1950187d51cb1488f874c17f050efcc70cf5a17a
tomnir <tomnir93@gmail.com>
2026-09-01 13:07:38 +0200

    initial draft

--- include/LightSources.h
+++ include/LightSources.h
@@ -7,14 +7,38 @@
 #include "include/ResourceLocator.h"
 #include <iostream>
 #include <filesystem>
+#include <vector>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include "Vec3.h"
+
+struct PointLight
+{
+    Vec3 position, color;
+    GLfloat attenuation;
+    GLfloat intensity;
+};
+
+struct SpotLight
+{
+    Vec3 position, color, direction;
+    GLfloat intensity;
+};
 
 class LightSources
 {
 private:
     int m_numberOfPointLights;
     int m_numberOfSpotlights;
+    std::vector<std::shared_ptr<PointLight>> m_pointLights;
+    std::vector<std::shared_ptr<SpotLight>> m_spotLights;
+public:
+    LightSources();
+    void addSpotLight(const SpotLight& spotLight);
+    void addPointLight(const PointLight& pointLight);
+    void clearAllLights();
+    void uploadLights();
 };
 
 
