cd8570c3cc23b757551b29f269838962d2fcc116
tomnir <tomnir93@gmail.com>
2026-09-02 17:43:48 +0200

    adjusted constructor to reflect header changes, added error handling for max amount of lights, added dirtying of removed point and spot light pointers, added first draft of uploadLights()

--- src/utilities/LightSources.cpp
+++ src/utilities/LightSources.cpp
@@ -4,32 +4,41 @@
 
 #include "../../include/LightSources.h"
 
-LightSources::LightSources()
-    : m_numberOfPointLights {0}
-    , m_numberOfSpotlights {0}
-{
-}
+LightSources::LightSources() = default;
 
 void LightSources::addPointLight(const PointLight& pointLight)
 {
+    if (m_pointLights.size() >= 10) throw std::runtime_error("Max point lights exceeded. Max is 10.");
     m_pointLights.push_back(std::make_shared<PointLight>(pointLight));
 
 }
 
 void LightSources::addSpotLight(const SpotLight& spotLight)
 {
+    if (m_pointLights.size() >= 2) throw std::runtime_error("Max spot lights exceeded. Max is 2.");
     m_spotLights.push_back(std::make_shared<SpotLight>(spotLight));
 }
 
 void LightSources::clearAllLights()
 {
+    for (const auto& pointLight : m_pointLights) pointLight->setDirty();
+    for (const auto& spotLight : m_spotLights) spotLight->setDirty();
     m_pointLights.clear();
     m_spotLights.clear();
-    m_numberOfPointLights = 0;
-    m_numberOfSpotlights = 0;
 }
 
-void LightSources::uploadLights(GLuint shader)
+void LightSources::uploadLights(const GLuint& shader) const
 {
-
+    const GLint numPointLightsLoc = glGetUniformLocation(shader, "numPointLights");
+    const GLint numSpotLightsLoc = glGetUniformLocation(shader, "numSpotLights");
+    glUniform1f(numPointLightsLoc, m_pointLights.size());
+    glUniform1f(numSpotLightsLoc, m_spotLights.size());
+    for (size_t i = 0; i < m_pointLights.size(); ++i)
+    {
+        m_pointLights[i]->upload(shader, i);
+    }
+    for (size_t j = 0; j < m_spotLights.size(); ++j)
+    {
+        m_spotLights[j]->upload(shader, j);
+    }
 }
