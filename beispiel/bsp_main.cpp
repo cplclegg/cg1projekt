1c6dac604e1c35d4c1c9ca050b43cd2439012a88
tomnir <tomnir93@gmail.com>
2026-09-08 16:55:25 +0200

    lots of experimentation with the object loading and positoning

--- beispiel/bsp_main.cpp
+++ beispiel/bsp_main.cpp
@@ -16,23 +16,91 @@ int main()
     // shader erdkugel
     ShaderProgram earthshader {"beispiel/shaders/vertexShader.glsl",
         "beispiel/shaders/fragmentShader.glsl"};
-
+    //shader tisch
+    ShaderProgram tableshader {"beispiel/shaders/vertexShader.glsl",
+    "beispiel/shaders/fragmentShader.glsl"};
+    // shader kerze
+    ShaderProgram candleshader {"beispiel/shaders/vertexShader.glsl",
+"beispiel/shaders/fragmentShader.glsl"};
+    // shader flamme
+    ShaderProgram flameshader {"beispiel/shaders/vertexShader.glsl",
+"beispiel/shaders/fragmentShader.glsl"};
     // texturen erdkugel
-    TextureData earth_day {"beispiel/textures/earth_day.png"};
+    TextureData earth_day {"beispiel/textures//table/TabMat_baseColor.jpeg"};
     earth_day.createTexture();
-    TextureData earth_clouds {"beispiel/textures/earth_clouds.png"};
+    TextureData earth_clouds {"beispiel/textures/table/TabMat_specularf0.png"};
     earth_clouds.createTexture();
 
     // erdkugel object laden
-    ObjectData earth_geometry {"beispiel/objects/earth.obj"};
+    ObjectData earth_geometry {"beispiel/objects/altar/altar_table.obj"};
+    ObjectData candle1_geometry {"beispiel/objects/candle/candle1.obj"};
+    ObjectData candle_flame_geometry {"beispiel/objects/candle/candle_flame.obj"};
 
+    //
+    TextureData empty {};
+    TextureData c1_base {"beispiel/textures/candles/Candle_1_baseColor.png"};
+    c1_base.createTexture();
+    TextureData c1_normal {"beispiel/textures/candles/Candle_1_normal.png"};
+    c1_normal.createTexture();
+    TextureData cf_base {"beispiel/textures/candles/Flame_baseColor.png"};
+    cf_base.createTexture();
+    TextureData cf_emissive {"beispiel/textures/candles/Flame_baseColor.png"};
+    cf_emissive.createTexture();
+
+    Material candle_fmat {
+        flameshader.getID(),
+        cf_base,
+        empty,
+        empty,
+        empty,
+        cf_emissive
+    };
+
+    Material candle1mat {
+        candleshader.getID(),
+        c1_base,
+        empty,
+        c1_normal,
+        empty,
+        empty
+    };
     // material-object zusammenfassen
     Material earth_mat {
         earthshader.getID(),
         earth_day,
+        TextureData {},
+        TextureData {},
         earth_clouds,
+        TextureData {}
+    };
+
+    Renderable candle1_data {
+        candle1_geometry,
+        candle1mat
+    };
+
+    Renderable candle_fdata {
+        candle_flame_geometry,
+        candle_fmat
+    };
+
+
+
+    SceneNode candle1_1 {candle1_data};
+    SceneNode candle1_1_f {candle_fdata};
+    candle1_1.translate(Vec3{1.0993350744247437, 0.23523379862308502, -1.183178186416626});
+    candle1_1_f.translate(Vec3{0.0, 0.225, 0.0});
+    candle1_1.addChild(candle1_1_f);
+    SceneNode candle1_2_f {candle_fdata};
+
+    SceneNode candle1_2 {candle1_data};
+    candle1_2.translate(Vec3{-0.3676091432571411, 0.23523379862308502, -1.183178186416626});
+    Material table_mat {
+        earthshader.getID(),
+        earth_day,
         TextureData {},
         TextureData {},
+        earth_clouds,
         TextureData {}
     };
 
@@ -40,7 +108,8 @@ int main()
     Renderable earth_data {earth_geometry, earth_mat};
     // renderable in scene node einbauen
     SceneNode earth {earth_data};
-
+    earth.addChild(candle1_1);
+    earth.addChild(candle1_2);
     //
     // cube map
     //
@@ -89,6 +158,7 @@ int main()
         view.lookAt(eye, center, up);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         earth.draw(transform, view, projection, 0, lights);
+        //candle1_1.draw(transform, view, projection, 0, lights);
         skybox.draw(projection, view);
         glfwPollEvents();
         glfwSwapBuffers(window);
